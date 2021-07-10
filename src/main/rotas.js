8b97f2f6b741547170c14ef923e25f7564470135
Epifanio CASSAMA GONCALVES <epifaju@gmail.com>
2021-07-10 23:59:36 +0200

    suite minhas finanças

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import {Route, Switch, HashRouter} from 'react-router-dom';
-import Login from '../views/login';
-import CadastroUsuario from '../views/cadastroUsuario';
+import { Route, Switch, HashRouter } from "react-router-dom";
+import Login from "../views/login";
+import CadastroUsuario from "../views/cadastroUsuario";
+import Home from "../views/home";
 
 function Rotas() {
-    return (
-        <HashRouter>
-            <Switch>
-                <Route path="/login" component={Login}/>
-                <Route path="/cadastro-usuarios" component={CadastroUsuario}/>
-            </Switch>
-        </HashRouter>
-    )
-
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/cadastro-usuarios" component={CadastroUsuario} />
+      </Switch>
+    </HashRouter>
+  );
 }
-export default Rotas
+export default Rotas;
